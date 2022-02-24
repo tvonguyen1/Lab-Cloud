62a5018ccc22c28b507f11a235b5315b9fef5b93
tvonguyen1 <nhattienvo21@gmail.com>
2022-02-24 01:42:24 -0500

    update queries

--- Cloud Lab/main/login.py
+++ Cloud Lab/main/login.py
@@ -4,54 +4,48 @@ Created on Feb 23, 2022
 @author: nhatt
 '''
 import mysql.connector
+import exception
 from mysql.connector import Error
-from main import user, exception
 import traceback
 
-try:
-    cnx = mysql.connector.connect(host='cloud-lab-mysql.mysql.database.azure.com',
-                                  user='sev_ad', 
-                                  passwd='Nhattienvo21!',
-                                  db='cloud_lab_sys')
-    
-except mysql.connector.Error as e:
-    if e.errno == mysql.connector.errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with username or Password")
-    elif e.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
-        print("Database Does not exist")
-    else:
-        print(e)
-        
-        
-cursor = cnx.cursor()
 
-username = input("Enter username: ")
-passwd = input("Enter password: ")
+def login():
+    try:
+        cnx = mysql.connector.connect(host='cloud-lab-mysql.mysql.database.azure.com',
+                                      user='sev_ad',
+                                      passwd='Nhattienvo21!',
+                                      db='cloud_lab_sys')
 
-statement = "SELECT count(*) FROM cloud_lab_sys.user_login WHERE login_name = %s AND password = %s"
+    except mysql.connector.Error as e:
+        if e.errno == mysql.connector.errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with username or Password")
+        elif e.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
+            print("Database Does not exist")
+        else:
+            print(e)
 
-vals = (username,passwd)
+    cursor = cnx.cursor()
+
+    username = input("Enter username: ")
+    passwd = input("Enter password: ")
+
+    statement = "SELECT * FROM cloud_lab_sys.user_login WHERE login_name = %s AND password = %s"
+
+    vals = (username, passwd)
+
+    try:
+        cursor.execute(statement, vals)
+        account = cursor.fetchone()
+
+        if account:
+            print("Successfully login")
+        else:
+            raise exception.valueError
+
+    except exception.valueError:
+        print("username or password is incorrect")
+    finally:
+        cnx.close()
 
 
-
-try:
-    count = cursor.execute(statement,vals)
-    cnx.commit()
-    
-    
-    
-    if count == 0:
-        raise exception.valueError(Error)
-    
-    print("Succesfully login!")
-
-except exception.valueError:
-    print("Username or password is incorrect")
-    
-    
-finally:
-    cursor.close()
-    cnx.close()
-    
-    
-    
+login()
--- Cloud Lab/main/register.py
+++ Cloud Lab/main/register.py
@@ -4,51 +4,45 @@ Created on Feb 22, 2022
 @author: nhatt
 '''
 from datetime import date
-from main import user, exception
 import mysql.connector
 from mysql.connector import Error
 import traceback
 
-try:
-    cnx = mysql.connector.connect(host='cloud-lab-mysql.mysql.database.azure.com',
-                                  user='sev_ad', 
-                                  passwd='Nhattienvo21!',
-                                  db='cloud_lab_sys')
-    
-except mysql.connector.Error as e:
-    if e.errno == mysql.connector.errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with username or Password")
-    elif e.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
-        print("Database Does not exist")
-    else:
-        print(e)
-        
-        
-cursor = cnx.cursor()
 
-username = input("Enter username: ")
-passwd = input("Enter password: ")
+def register():
+    try:
+        cnx = mysql.connector.connect(host='cloud-lab-mysql.mysql.database.azure.com',
+                                      user='sev_ad',
+                                      passwd='Nhattienvo21!',
+                                      db='cloud_lab_sys')
 
-    
-statement = "INSERT INTO cloud_lab_sys.user_login (login_name,password) VALUES (%s, %s)"
-    
-vals = (username, passwd)
-    
-try:
-    cursor.execute(statement,vals)
-    cnx.commit()
-    
-except Error:
-    print("Username already taken")
-        
-         
-finally:
-    cursor.close()
-    cnx.close()
-    
-    
-    
-    
-      
-   
-        
+    except mysql.connector.Error as e:
+        if e.errno == mysql.connector.errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with username or Password")
+        elif e.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
+            print("Database Does not exist")
+        else:
+            print(e)
+
+    cursor = cnx.cursor()
+
+    username = input("Enter username: ")
+    passwd = input("Enter password: ")
+
+    statement = "INSERT INTO cloud_lab_sys.user_login (login_name,password) VALUES (%s, %s)"
+
+    vals = (username, passwd)
+
+    try:
+        cursor.execute(statement, vals)
+        cnx.commit()
+
+    except Error:
+        print("Username already taken")
+
+    finally:
+        cursor.close()
+        cnx.close()
+
+
+register()
