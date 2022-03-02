adc09c99540102eb92e495164ae864a340ef3aab
tvonguyen1 <nhattienvo21@gmail.com>
2022-03-02 17:47:26 -0500

    update index

--- Cloud Lab/main/index.py
+++ Cloud Lab/main/index.py
@@ -6,6 +6,7 @@ import time
 import calendar
 import quopri
 import chardet
+import ndjson
 from bs4 import BeautifulSoup
 import logging
 
@@ -55,6 +56,7 @@ def create_index():
                     "type": {"type": "string", "index": "not_analyzed"},
                     "size": {"type": "string", "index": "not_analyzed"},
                     "location": {"type": "string", "index": "not_analyzed"},
+                    "file-id": {"type": "string", "index": "not_analyzed"},
                     "owner": {"type": "string", "index": "not_analyzed"},
                     "date_create": {"type": "date"},
                 },
@@ -75,3 +77,34 @@ def create_index():
 
 
 total_uploaded = 0
+
+def convert_file_to_json(blob):
+    json_data_string = blob.download_as_string()
+    json_data = ndjson.loads(json_data_string)
+
+
+
+def upload_batch(upload_data):
+    if tornado.options.options.dry_run:
+        logging.info("Dry run, not uploading")
+        return
+    upload_data_txt = ""
+    for item in upload_data:
+        cmd = {'index': {'_index': tornado.options.options.index_name, '_type': 'file', '_id': item['message-id']}}
+        try:
+            json_cmd = json.dumps(cmd) + "\n"
+            json_item = json.dumps(item) + "\n"
+        except:
+            logging.warn('Skipping mail with message id %s because of exception converting to JSON (invalid characters?).' % item['message-id'])
+            continue
+        upload_data_txt += json_cmd
+        upload_data_txt += json_item
+
+    request = HTTPRequest(tornado.options.options.es_url + "/_bulk", method="POST", body=upload_data_txt, request_timeout=240, headers={"Content-Type": "application/json"})
+    response = http_client.fetch(request)
+    result = json.loads(response.body)
+
+    global total_uploaded
+    total_uploaded += len(upload_data)
+    res_txt = "OK" if not result['errors'] else "FAILED"
+    logging.info("Upload: %s - upload took: %4dms, total messages uploaded: %6d" % (res_txt, result['took'], total_uploaded))
